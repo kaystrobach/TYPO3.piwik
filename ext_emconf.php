edaf869c4190dcf05ac255a3fe537364259a9415
Alexander Stehlik <alexander.stehlik@gmail.com>
2020-02-06 16:40:42 +0100

    [CLEANUP] Cleanup ext_emconf.php (remove obsolete, reformat)

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,51 +1,19 @@
 <?php
-
-/***************************************************************
- * Extension Manager/Repository config file for ext "piwik".
- *
- * Auto generated 11-03-2013 20:23
- *
- * Manual updates:
- * Only the data in the array - everything else is removed by next
- * writing. "version" and "dependencies" must not be touched!
- ***************************************************************/
-
-$EM_CONF[$_EXTKEY] = array (
-	'title' => 'Piwik - web analytics, new API',
-	'description' => 'Adds Piwik JS-Code (http://piwik.org/) to your pages',
-	'category' => 'fe',
-	'shy' => 0,
-	'version' => '4.0.0-dev',
-	'dependencies' => '',
-	'conflicts' => '',
-	'priority' => '',
-	'loadOrder' => '',
-	'module' => '',
-	'state' => 'stable',
-	'uploadfolder' => 0,
-	'createDirs' => '',
-	'modify_tables' => '',
-	'clearcacheonload' => 0,
-	'lockType' => '',
-	'author' => 'Kay Strobach',
-	'author_email' => 'kay.strobach@typo3.org',
-	'author_company' => 'www.kay-strobach.de',
-	'CGLcompliance' => '',
-	'CGLcompliance_note' => '',
-	'constraints' => 
-	array (
-		'depends' => 
-		array (
-			'typo3' => '9.0.0-9.99.99',
-		),
-		'conflicts' => 
-		array (
-		),
-		'suggests' => 
-		array (
-		),
-	),
-	'suggests' => 
-	array (
-	),
-);
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Piwik - web analytics, new API',
+    'description' => 'Adds Piwik JS-Code (http://piwik.org/) to your pages',
+    'category' => 'fe',
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearcacheonload' => 0,
+    'author' => 'Kay Strobach',
+    'author_email' => 'kay.strobach@typo3.org',
+    'author_company' => 'www.kay-strobach.de',
+    'version' => '4.0.0-dev',
+    'constraints' => [
+        'depends' => ['typo3' => '9.0.0-9.99.99'],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
